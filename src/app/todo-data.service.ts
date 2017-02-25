8b631d2f632d9558c7fae258f12c80a12e92fd46
erikmura44 <erikmura44@gmail.com>
2017-02-25 12:37:37 -0700

    first commit

---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoDataService {
+
+    lastId: number = 0;
+
+    todos: Todo[] = [];
+
+
+  constructor() {
+}
+
+addTodo(todo: Todo): TodoDataService {
+    if (!todo.id){
+        todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+}
+
+
+
+}
