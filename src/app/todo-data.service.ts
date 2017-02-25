66b17574bb9450289be18ccab21c431f2304e7cb
erikmura44 <erikmura44@gmail.com>
2017-02-25 12:41:45 -0700

    C R U D added

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -12,6 +12,7 @@ export class TodoDataService {
   constructor() {
 }
 
+//Simulate POST /todos
 addTodo(todo: Todo): TodoDataService {
     if (!todo.id){
         todo.id = ++this.lastId;
@@ -20,6 +21,42 @@ addTodo(todo: Todo): TodoDataService {
     return this;
 }
 
+//Simulate DELETE /todos/:id
+deleteTodoById(id: number): TodoDataService {
+    this.todos = this.todos
+        .filter(todo => todo.id !== id);
+    return this;
+}
+
+//Simulate PUT /todos/:id
+updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo){
+        return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+}
+
+// Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo){
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
 
 
 }
